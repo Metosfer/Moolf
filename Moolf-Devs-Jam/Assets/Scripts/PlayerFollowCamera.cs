670e85ceec8949225360972520c6a82d79b4c982
Metosfer <metehanbozdagg@hotmail.com>
2024-07-27 15:54:17 +0300

    PostProcces_Setup

--- Moolf-Devs-Jam/Assets/Scripts/PlayerFollowCamera.cs
+++ Moolf-Devs-Jam/Assets/Scripts/PlayerFollowCamera.cs
@@ -1,25 +1,16 @@
 using UnityEngine;
 
-public class PlayerFollowCamera : MonoBehaviour
+public class PlayerFollow : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offset;
-    public float bumpIntensity = 0.1f;
-    public float bumpSpeed = 2f;
+    public Transform player;   // Oyuncunun transformu
+    public Vector3 offset;    // Kamera ile oyuncu arasýndaki mesafe
 
-    void LateUpdate()
+    private void Update()
     {
-        transform.position = player.position + offset;
-
-        // Bumping effect
-        if (player.GetComponent<CharacterController>().velocity.magnitude > 0)
+        if (player != null)
         {
-            float bumpOffset = Mathf.Sin(Time.time * bumpSpeed) * bumpIntensity;
-            transform.localPosition = new Vector3(transform.localPosition.x, offset.y + bumpOffset, transform.localPosition.z);
-        }
-        else
-        {
-            transform.localPosition = new Vector3(transform.localPosition.x, offset.y, transform.localPosition.z);
+            // Kamerayý oyuncunun konumuna göre konumlandýr
+            transform.position = player.position + offset;
         }
     }
 }
